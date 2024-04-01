1d2e4052dceeaa66852ed6d20e3c565bac616a88
Mariam Shahir <mariamshahir2000@gmail.com>
2024-04-01 21:27:23 +0200

    Finished home screen.

---
+++ lib/homescreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String routeName = "homescreen";
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.black
+      ),
+    );
+  }
+}
